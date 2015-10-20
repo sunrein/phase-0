1c8497f877c43cb0f7721afdf6512deee3ee3125
Sabrina Unrein <sabrina.unrein@gmail.com>
2015-10-20 12:59:16 -0600

    Math methods

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+  num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+  num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+  num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ num_1.to_f / num_2.to_f
+end
+
+puts add(10,2)
+puts subtract(10,2)
+puts multiply(10,2)
+puts divide(10,2)
