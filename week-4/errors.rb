648ee50167ef4cf58fe27827fed900cf74fd9a69
Sabrina Unrein <sabrina.unrein@gmail.com>
2015-10-21 10:07:37 -0600

    Update errors.rb

--- week-4/errors.rb
+++ week-4/errors.rb
@@ -1,7 +1,7 @@
 # Analyze the Errors
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [by myself].
+# I spent [1] hour on this challenge.
 
 # --- error -------------------------------------------------------
 
